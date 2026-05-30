1354ccf3960b6d8d09480373a2a7fd370d8a584f
thenry03 <egc0033@gmail.com>
2026-05-30 17:28:42 +0200

    Feat(renderer): render cube with MVP transforms and depth test

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,133 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "core/Window.hpp"
+#include "renderer/Shader.hpp"
 
 constexpr int width = 800;
 constexpr int height = 600;
+
+// Define
+unsigned int VAO, VBO, EBO;
+
+void defineCube()
+{
+    static const float vertices[] = {
+        // Front
+        -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+         1.0f, 1.0f,  1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,  1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+        // Back
+        -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+         1.0f, 1.0f,  -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,  -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f
+    };
+
+    static const unsigned int indexes[] = {
+        // Front
+        0, 1, 2,
+        0, 2, 3,
+        // Right
+        1, 5, 6,
+        6, 2, 1,
+        // Back
+        7, 6, 5,
+        5, 4, 7,
+        // Left
+        4, 0, 3,
+        3, 7, 4,
+        // Bottom
+        4, 5, 1,
+        1, 0, 4,
+        // Top
+        3, 2, 6,
+        6, 7, 3};
+
+    // Generate
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // Bind VAO
+    glBindVertexArray(VAO);
+
+    // Bind buffers
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+
+    // Send data
+    // Position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // Normal
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // Texture
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Unbind buffers
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbind VAO
+    glBindVertexArray(0);
+}
+
+void drawCube(const Shader &shader, const Window &window)
+{
+    // Transformations
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat3 normalMatrix = glm::mat3(glm::transpose(glm::inverse(model)));
+
+    glm::mat4 view = glm::lookAt(
+        glm::vec3(3.0f, 2.0f, 5.0f),
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f));
+
+    glm::mat4 projection = glm::perspective(
+        glm::radians(45.0f),
+        (float)window.getWidth() / (float)window.getHeight(),
+        0.1f,
+        100.0f);
+
+    // Send uniforms
+    shader.setMat3("normalMatrix", normalMatrix);
+    shader.setMat4("model", model);
+    shader.setMat4("view", view);
+    shader.setMat4("projection", projection);
+
+    // Draw calls
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
+    glBindVertexArray(0);
+}
 
 int main()
 {
     // Safe stack initialization
     Window window(width, height, "VoxelCPP");
+    Shader shader("shaders/shader.vert", "shaders/shader.frag");
 
     // Set VSync off
     window.setVSync(false);
 
-    while(!window.shouldClose())
+    defineCube();
+    glEnable(GL_DEPTH_TEST);
+
+    while (!window.shouldClose())
     {
         window.clear();
+
+        shader.bind();
+        drawCube(shader, window);
 
         window.swapBuffers();
         window.pollEvents();
